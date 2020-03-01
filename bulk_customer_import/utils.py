cfc49b6d111c0d2b752bb5aff359a4892a44832e
Simon Merrick <simonmerrick@catalyst.net.nz>
2020-03-02 11:45:55 +1300

    Revert "Added whitespace stripping to csv parser (#10)"
    This reverts commit 2e83355ba2ddfc0c98120e2adfdc49ffb6a3d4c1.

--- bulk_customer_import/utils.py
+++ bulk_customer_import/utils.py
@@ -6,7 +6,5 @@ def parse_csv(filename):
     with open(filename, "r") as f:
         reader = csv.reader(f, delimiter=",", quotechar='"')
         for row in reader:
-            for item in row:
-                row[item] = row[item].strip()
             output.append(row)
     return output
