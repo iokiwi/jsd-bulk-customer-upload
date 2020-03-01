2e83355ba2ddfc0c98120e2adfdc49ffb6a3d4c1
Mustapha Garba <mgarba88@gmail.com>
2020-03-01 23:40:55 +0100

    Added whitespace stripping to csv parser (#10)

--- bulk_customer_import/utils.py
+++ bulk_customer_import/utils.py
@@ -6,5 +6,7 @@ def parse_csv(filename):
     with open(filename, "r") as f:
         reader = csv.reader(f, delimiter=",", quotechar='"')
         for row in reader:
+            for item in row:
+                row[item] = row[item].strip()
             output.append(row)
     return output
